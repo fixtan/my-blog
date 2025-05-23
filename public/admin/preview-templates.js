427cd2ffa73486db416f7567aeec7da349567853
fixtan <fixjp@icloud.com>
2025-05-23 18:41:16 +0900

    Update custom CSS

--- public/admin/preview-templates.js
+++ public/admin/preview-templates.js
@@ -1,6 +1,7 @@
 import CMS from "netlify-cms-app";
 
 // カスタムCSSを読み込む
-CMS.registerPreviewStyle("/css/custom.css");
+//CMS.registerPreviewStyle("/css/custom.css");
+CMS.registerPreviewStyle("/css/custom-preview.css");
 
 // 必要に応じてテーマのスタイルも追加（例: PaperModのスタイル）
