304b92883e22fcb8c6732fdb6c72b7fb1838156a
fixtan <fixjp@icloud.com>
2025-05-23 19:40:36 +0900

    Fix: enable CSS in preview

--- public/admin/preview-templates.js
+++ public/admin/preview-templates.js
@@ -1,4 +1,5 @@
 import CMS from "netlify-cms-app";
 
 // 既存の CSS を読み込む
-CMS.registerPreviewStyle("/css/custom.css", { raw: false });
+//CMS.registerPreviewStyle("/css/custom.css", { raw: false });
+CMS.registerPreviewStyle("/css/custom.css");
