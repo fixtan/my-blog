85650bb046de59c80486b093d96fbd3c52cdd579
fixtan <fixjp@icloud.com>
2025-05-24 11:35:08 +0900

    Fix CSS preview 2025-05-24_08-23

--- static/admin/preview-templates.js
+++ static/admin/preview-templates.js
@@ -1,5 +1,5 @@
 const CMS = window.CMS;
 
 // 最小限のスタイルのみ追加
-//CMS.registerPreviewStyle("/css/custom.css");  // public/css/custom.css に合わせてパス指定
+CMS.registerPreviewStyle("/css/custom.css");  // public/css/custom.css に合わせてパス指定
 CMS.init();
